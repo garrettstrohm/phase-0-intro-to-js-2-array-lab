7ee6721577ab930cf194486263a247a19cc32d2f
Garrett <garres4@vt.edu>
2021-09-14 11:03:43 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield",
+];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name){
+   const copyCats = cats.slice();
+   copyCats.push(name);
+   return copyCats;
+}
+
+function prependCat(name){
+    const copyCats = cats.slice();
+    copyCats.unshift(name);
+    return copyCats;
+ }
+
+ function removeLastCat(name){
+    const copyCats = cats.slice();
+    copyCats.pop(name);
+    return copyCats;
+ }
+
+ function removeFirstCat(name){
+    const copyCats = cats.slice();
+    copyCats.shift(name);
+    return copyCats;
+ }
